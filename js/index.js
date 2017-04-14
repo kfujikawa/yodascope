151405ae614ec8d07fd21e942e082a8af46ad812
Kinuyo Fujikawa <kinuyofujikawa@gmail.com>
2017-04-14 14:46:39 -0700

    added babel-polyfill

--- js/index.js
+++ js/index.js
@@ -1,23 +1,24 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const Provider = require('react-redux');
+require("babel-polyfill");
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
+// const React = require('react');
+// const ReactDOM = require('react-dom');
+// const Provider = require('react-redux');
 
-const store = require('./store');
-const Horoscope = require('./components/Horoscope');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
-// import store from './store';
-// import Horoscope from './components/Horoscope';
+// const store = require('./store');
+// const Horoscope = require('./components/Horoscope');
+
+import store from './store';
+import Horoscope from './components/Horoscope';
 
 // import * as actions from './actions/index';
 // import * as reducers from './reducers/index';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Horoscope />
-  </Provider>,
+// ReactDOM.render(routes, document.getElementById('app'));
+
+ReactDOM.render(<Provider store={store}><Horoscope /></Provider>,
   document.getElementById('app')
 );

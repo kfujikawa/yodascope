dff237ed816138a94b90329ea83155a3f964bb58
Kinuyo Fujikawa <kinuyofujikawa@gmail.com>
2017-04-18 12:54:25 -0700

    added star effects CSS

--- server.js
+++ server.js
@@ -13,17 +13,17 @@ app.listen(PORT, function() {
   console.log('Example app listening on localhost:' + PORT);
 });
 
-let yoda = new YodaSpeak('2ZdLUzroC6mshVRTRBvJfsvCUzdYp16MK46jsnRHSID15Kx5W2');
+// let yoda = new YodaSpeak('2ZdLUzroC6mshVRTRBvJfsvCUzdYp16MK46jsnRHSID15Kx5W2');
 
-yoda.convert(
-  "I'm really happy for you, and I'm going to let you finish, but this is the best Node package of all time.",
-  function(err, result) {
-    if (!err) {
-      console.log(result.toString());
-    } else {
-      console.log(err);
-    }
-  }
-);
+// yoda.convert(
+//   "I'm really happy for you, and I'm going to let you finish, but this is the best Node package of all time.",
+//   function(err, result) {
+//     if (!err) {
+//       console.log(result.toString());
+//     } else {
+//       console.log(err);
+//     }
+//   }
+// );
 
 module.exports = app;

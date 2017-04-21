f4ffbe5c7a7fed9efdc2bca5ffdc14a359a6cd7d
Aleksandar Grbic <agrbicpp@gmail.com>
2017-04-21 18:27:38 +0200

    removed stuff from gh-pages

--- js/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-
-import thunk from 'redux-thunk';
-
-import { horoscopeReducer } from './reducers/index';
-
-const store = createStore(horoscopeReducer, applyMiddleware(thunk));
-
-export default store;
